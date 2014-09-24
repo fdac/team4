1af7e798db88d48d0946dfbe834e214236a6d369
dmercer2 <mercergm@David-Mercer.local>
2014-09-24 16:10:26 -0400

    final version of R code

--- Rcode.R
+++ Rcode.R
@@ -41,13 +41,12 @@ p.language <- p.language + geom_histogram()
 p.language
 rm(p.language)
 
-p.activity <- ggplot( data = d, aes(x = activity) ) + geom_histogram( binwidth = 50 )
-p.activity # most repositories are active for < 50 days - heavily skewed
-rm(p.activity)
+
 
 p.size <- ggplot ( data = d, aes(x = size) ) + geom_histogram()
 p.size
 rm(p.size)
+
 
 
 
@@ -62,27 +61,41 @@ rm(p.size)
 # technologies used
 head(sort(unique(d$scm), decreasing = TRUE), 30)
   #html5 hg git then just numbers
-a <- length(d$scm[ which(d$scm == "hg") ])
-b <- length(d$scm[ which(d$scm == "git") ])
-c <- length(d$scm[!d$scm %in% c("hg", "git") ])
-a + b + c
-nrow(d)
+length(d$scm[ which(d$scm == "hg") ])
+length(d$scm[ which(d$scm == "git") ])
+length(d$scm[!d$scm %in% c("hg", "git") ])
 
-# most popular languages
+
+#### most popular languages
 length(unique(d$language))
-table(d$language)
+head(sort(table(d$language), decreasing = TRUE), 10)
+head(sort(table(d$language), decreasing = TRUE), 10) / nrow(d)
 
-# repo size stats
+
+#### repo size stats
 summary(d$size)
 mean(d$size)
-qplot(size, data = d, geom = "histogram")
+qplot(log(size), data = d, geom = "histogram")
+head(d$size[order(d$size, decreasing = TRUE)])
 
-# repo lifetime stats
+length(d$size[ which(d$size >= 1.25e8)])/nrow(d)
+
+length(which(d$size == 0))
+length(which(d$size < 1000))
+
+#### repo lifetime stats
 summary(d$activity)
 qplot(activity, data = d, geom = "histogram")
 
 # still active repos
 length(which(Sys.Date() - d$updated_on < 30))
+ggplot( data = d, aes(x = activity) ) + geom_histogram( binwidth = 50 )
+ # most repositories are active for < 50 days - heavily skewed
+qplot( activity, data = d[which(d$activity<51),], geom = "histogram")
+ # looks like most repos are created and never updated or updated on the same day
+length(which(d$activity == 0))
+ # 175,308 repos
+
 
 
 
@@ -95,6 +108,20 @@ length(unique(d$created_on))
 library(dplyr)
 date.groups <- group_by(d[order(d$created_on),], created_on)
 head(date.groups)
-group_size(date.groups)
+date.count <- summarize(date.groups, repositories = n())
+date.count$total <- cumsum(date.count$repositories)
+date.count
 
+qplot(x = created_on, y = total, data = date.count, geom = "line")
+p <- ggplot(data = date.count, aes(x = created_on, y = total)) +
+  geom_line() +
+  xlab('Date') +
+  ylab('Total Repositories') +
+  ggtitle('Bitbucket Repository Growth')
 
+## number of unique owners
+length(unique(d$owner))
+
+### active repos - updated at most 30 days ago
+length(which(as.Date("2014-09-18", format = "%Y-%m-%d") - d$updated_on < 31))
+length(which(as.Date("2014-09-18", format = "%Y-%m-%d") - d$updated_on < 31))/nrow(d)
